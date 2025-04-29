ccb5b099c1865e427751d1a0dcfa8aae6ace042e
Kasyymov Ilyar <kasyymov.ilyar@gmail.com>
2025-04-29 13:50:12 +0300

    fix title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	title: "Hash2Cash",
-	tagline: "Тестовая документация проекта",
+	tagline: "Тестовая документация проекта hash2cash",
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
