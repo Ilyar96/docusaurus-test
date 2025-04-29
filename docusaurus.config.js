d82d7ea29c8bc0d386c47aad3d1d368b29501a03
Ilyar Kasyymov <113968348+Ilyar96@users.noreply.github.com>
2025-04-29 14:39:31 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Hash2Cash",
+	title: "Hash2Cash тест",
 	tagline: "Тестовая документация проекта hash2cash",
 	favicon: "img/favicon.ico",
 
